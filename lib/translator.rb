78f217a0c2c524d0698a2320d5761b9f39aa5246
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-16 06:08:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,9 +13,14 @@ def load_library
 end
 
 def get_japanese_emoticon
-  # code goes here
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
